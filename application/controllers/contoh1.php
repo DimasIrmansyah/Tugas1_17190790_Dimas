486a2ac8aafaf4dab41ffed79e810a962ce4fbcb
DimasIrmansyah <101915536+DimasIrmansyah@users.noreply.github.com>
2022-04-21 16:17:34 +0700

    Add files via upload

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+ <?php
+ class contoh1 extends CI_Controller
+ {
+     public function index()
+     {
+        echo "<h1> PERKENALKAN </h1>";
+        echo " Nama Saya Dimas Irmansyah <br> Saya Tinggal di Tangerang <br> Hobi saya tidur ";
+     }
+     
+ }
+ 
